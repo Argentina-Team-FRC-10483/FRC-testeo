df0182204e6caded952cdecb34f13d719e37b8ac
guadamurgo <guadazoemurgo@gmail.com>
2024-12-23 10:51:42 -0300

    agrege clases para cada mecanismo

--- src/main/java/frc/robot/Movimiento.java
+++ src/main/java/frc/robot/Movimiento.java
@@ -1,5 +1,7 @@
 package frc.robot;
 
 public class Movimiento {
-    
+    public static void movimientoPeriodico(){
+        
+    }
 }
---
+++ src/main/java/frc/robot/Recoleccion.java
@@ -0,0 +1,7 @@
+package frc.robot;
+
+public class Recoleccion {
+    public static void recoleccionPeriodico(){
+        
+    }
+}
