60a52e0a09cc980262bf69975cf925ad393e00ca
guadamurgo <guadazoemurgo@gmail.com>
2024-12-26 09:34:49 -0300

    cambios en robot herencia

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -66,5 +66,6 @@ public class Robot extends TimedRobot {
     Recoleccion.recoleccionPeriodico(null, null);
     Enganche.enganchePeriodico(joystick, motorLeftEnganche, motorRightEnganche);
 
+
   }
 }
