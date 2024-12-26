5665fe8a2ec21acb010d759cef38720f4f2f7727
guadamurgo <guadazoemurgo@gmail.com>
2024-12-26 09:52:08 -0300

    listo enganche

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,4 +1,5 @@
 package frc.robot;
+
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
@@ -10,57 +11,53 @@ public class Robot extends TimedRobot {
   // Motores lanzamiento
   private final Spark motorArribaLanzamiento = new Spark(0); // Puerto PWM 0
   private final Spark motorAbajoLanzamiento = new Spark(1); // Puerto PWM 1
-  
-  // Motores Enganche
-  private final Spark motorLeftEnganche =new Spark(6); // Puerto PWM 2
-  private final Spark motorRightEnganche =new Spark(7); // Puerto PWM 3
 
   // Joystick
   private final Joystick joystick = new Joystick(0); // Puerto USB 0
 
-  // boton para activar los motores lanzamiento
+  // Botón para activar los motores lanzamiento
   private final int botonActivarTiro = 1; // Botón 1 del joystick
 
-  //boton de disparo forzado
-  private final int botonDisparoForzado = 2; // botón 2 del joystick
+  // Botón de disparo forzado
+  private final int botonDisparoForzado = 2; // Botón 2 del joystick
 
-  // temporizador lanzamiento disparo forzado
+  // Temporizador lanzamiento disparo forzado
   private final Timer timer = new Timer();
 
-  // indicar si los motores lanzamiento están activos
+  // Indicar si los motores lanzamiento están activos
   private boolean estanActivos = false;
 
-  // sensor para detectar si hay un disco zona de lanzamiento
+  // Sensor para detectar si hay un disco en la zona de lanzamiento
   private final DigitalInput sensorDisco = new DigitalInput(0); // Puerto DIO 0
 
-  //Indica si se encuentra o no un disco zona de lanzamiento
-  private boolean hayDisco = false; //no importa que inicie en falso porque este valor se actualiza todo el tiempo
+  // Indica si se encuentra o no un disco en la zona de lanzamiento
+  private boolean hayDisco = false; // No importa que inicie en falso porque este valor se actualiza todo el tiempo
 
-  //MOVIMIENTO
-  //Motores movimiento
-  private Spark motorAdelanteIzquierdaMovimiento = new Spark(2); //Puerto PWM 2
-  private Spark motorAdelanteDerechaMovimiento = new Spark(3); //Puerto PWM 3
-  private Spark motorAtrasIzquierdaMovimiento = new Spark(4); //Puerto PWM 4
-  private Spark motorAtrasDerechaMovimiento = new Spark(5); //Puerto PWM 5
+  // Movimiento
+  private Spark motorAdelanteIzquierdaMovimiento = new Spark(2); // Puerto PWM 2
+  private Spark motorAdelanteDerechaMovimiento = new Spark(3); // Puerto PWM 3
+  private Spark motorAtrasIzquierdaMovimiento = new Spark(4); // Puerto PWM 4
+  private Spark motorAtrasDerechaMovimiento = new Spark(5); // Puerto PWM 5
 
-  //RECOLECCIÓN
-  // Motores de recolección
-  private Spark motorArribaRecoleccion = new Spark(6); //Puerto PWM 6
-  private Spark motorAbajoRecoleccion = new Spark(7); //Puerto PWM 7
+  // Recolección
+  private Spark motorArribaRecoleccion = new Spark(6); // Puerto PWM 6
+  private Spark motorAbajoRecoleccion = new Spark(7); // Puerto PWM 7
+
+  // Enganche
+  private Spark motorLeftEnganche = new Spark(8); // Puerto PWM 8
+  private Spark motorRightEnganche = new Spark(9); // Puerto PWM 9
 
   @Override
   public void robotPeriodic() {
-    //esto se esta actualizando todo el tiempo
-    hayDisco = sensorDisco.get(); // true si el sensor detecta el disco, false si no
+    // Esto se está actualizando todo el tiempo
+    hayDisco = sensorDisco.get(); // True si el sensor detecta el disco, false si no
   }
 
   @Override
   public void teleopPeriodic() {
-    
     Lanzamiento.lanzamientoPeriodico(motorArribaLanzamiento, motorAbajoLanzamiento, joystick, botonActivarTiro, botonDisparoForzado, timer, estanActivos, sensorDisco, hayDisco);
     Movimiento.movimientoPeriodico(joystick, motorAdelanteIzquierdaMovimiento, motorAdelanteDerechaMovimiento, motorAtrasIzquierdaMovimiento, motorAtrasDerechaMovimiento);
     Enganche.enganchePeriodico(joystick, motorLeftEnganche, motorRightEnganche);
-    Recoleccion.recoleccionPeriodico(Spark motorAbajoRecoleccion, Spark motorArribaRecoleccion);
-
+    Recoleccion.recoleccionPeriodico(motorAbajoRecoleccion, motorArribaRecoleccion);
   }
 }
